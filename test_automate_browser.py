86a7e2bd1bd1e5b7d2f8c1dca8c158b224813069
Okoa Kinsey <okoa.kinsey@stresearch.com>
2021-02-14 23:02:41 -0500

    click() only opens new page when --headless option is not set

--- test_automate_browser.py
+++ test_automate_browser.py
@@ -3,33 +3,63 @@ import io
 import unittest
 import contextlib
 from selenium import webdriver 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+BASE_URL = "https://www.duo.com"
 
 class AutomateBrowser(unittest.TestCase):
     def setUp(self):
         self.chrome_options = Options()
         self.chrome_options.add_argument("--headless")
+        self.chrome_options.add_argument("detach")
         self.chrome_options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
         self.driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), options=self.chrome_options)
 
     def test_search_target(self):
-        self.driver.get("https://www.duo.com")
-        self.driver.implicitly_wait(4)
-        assert "We're Duo." in self.driver.page_source
-        # self.driver.find_element_by_xpath('//a[@href="/product"]').click()
-        product = self.driver.find_element_by_xpath('//a[@href="/product"]')
-        ActionChains(self.driver).move_to_element(product).click()
-        print(f"test_automate_browser.py - product: {product}")
+        
+        self.driver.get(BASE_URL)
+        self.driver.implicitly_wait(10)
+        handle1 = self.driver.title
+        print(f"window 1 handle: {handle1}")
+        assert "We're Duo." in self.driver.page_source 
+
+        product = self.driver.find_element_by_xpath('//a[@href="/product"]') 
+
+        # *** keep this in the code for reference
+        # ActionChains(self.driver).move_to_element(product).click() *** keep this in the code for reference
+        # self.driver.execute_script("document.querySelector(\"[href='/product']\").click()")
+        # time.sleep(3) 
+        # ******
+
+        self.driver.get(BASE_URL+'/product')
+        for handle in self.driver.window_handles:
+            self.driver.switch_to.window(handle)
+        handle2 = self.driver.window_handles
+        print(f"test_automate_browser.py - window 2 handle: {self.driver.title}")
+        page_heading = self.driver.find_element_by_css_selector(".header-basic__content.content h1")
+
+        
+        # WebDriverWait(self.driver, 10).until(lambda x:x.find_element_by_css_selector(".header-basic__content.content"))
+
+        assert "Access Security" in page_heading.text
+        
 
     def tear_down(self):
         self.driver.close() 
 
 if __name__ == "__main__":
-    import __main__ 
-    suite = unittest.TestLoader().loadTestsFromModule(__main__)
-    with io.StringIO() as buf:
-        with contextlib.redirect_stdout(buf):
-            unittest.TextTestRunner(stream=buf).run(suite)
-        print("*** CAPTURED TEXT ***:\n%s" % buf.getvalue())
+    unittest.main()
+    
+    # *** keep this in the code for reference
+    # import __main__ 
+    # suite = unittest.TestLoader().loadTestsFromModule(__main__)
+    # with io.StringIO() as buf:
+    #     with contextlib.redirect_stdout(buf):
+    #         unittest.TextTestRunner(stream=buf).run(suite)
+    #     print("*** CAPTURED TEXT ***:\n%s" % buf.getvalue())
+    # ******
